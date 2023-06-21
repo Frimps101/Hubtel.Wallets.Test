ec296a88001b962ac3cc128cd234a15e01cbb469
Josephine Frimpomaa Kwakye <jkwakye001@gmail.com>
2023-06-21 06:01:50 +0000

    first commit

---
+++ Hubtel.Wallets.Api/Controllers/WalletController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Hubtel.Wallets.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletController : ControllerBase
+    {
+        public async Task createWallet()
+        {
+
+        }
+
+        public async Task GetWallet()
+        {
+            if (!ModelState.IsValid)
+            {
+
+            }
+            
+        }
+    }
+}
